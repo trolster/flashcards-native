9a84421fb3e2a934e36f5fac0211b3c461a82d19
Mikkel Trolle Larsen <trolster2@gmail.com>
2017-10-13 03:58:06 +0200

    refactor: remove REMOVE_CARD

--- actions/index.js
+++ actions/index.js
@@ -1,7 +1,6 @@
 export const RECEIVE_DECKS = "RECEIVE_DECKS";
 export const ADD_DECK = "ADD_DECK";
 export const ADD_CARD = "ADD_CARD";
-export const REMOVE_CARD = "REMOVE_CARD";
 
 export function receiveDecks(decks) {
   return {
@@ -22,13 +21,5 @@ export function addCard(deck, card) {
     type: ADD_CARD,
     deck,
     card
-  };
-}
-
-export function removeCard(deck, cardIndex) {
-  return {
-    type: REMOVE_CARD,
-    deck,
-    cardIndex
   };
 }
--- reducers/index.js
+++ reducers/index.js
@@ -1,4 +1,4 @@
-import { RECEIVE_DECKS, ADD_DECK, ADD_CARD, REMOVE_CARD } from "../actions";
+import { RECEIVE_DECKS, ADD_DECK, ADD_CARD } from "../actions";
 import testState from "../utils/testState.json";
 
 const initialState = {
@@ -15,8 +15,6 @@ function deck(state = initialState, action) {
     case ADD_DECK:
       return {};
     case ADD_CARD:
-      return {};
-    case REMOVE_CARD:
       return {};
     default:
       return state;
